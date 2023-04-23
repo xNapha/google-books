e6b74b85bdd6c24eaaf1f429b52a779ccfc63081
xNapha <nathancai1997@gmail.com>
2023-04-23 22:46:23 +1000

    added local storage api

--- src/contexts/FavouritesProvider.jsx
+++ src/contexts/FavouritesProvider.jsx
@@ -1,8 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { createContext } from "react";
 export const FavouritesContext = createContext();
 const FavouritesProvider = ({ children }) => {
-    const [favouriteBooks, setFavouriteBooks] = useState([]);
+    if (!localStorage.googleBooks) {
+        localStorage.setItem("googleBooks", JSON.stringify([]));
+    }
+    const localStorageBooks = JSON.parse(localStorage.getItem("googleBooks"));
+    const [favouriteBooks, setFavouriteBooks] = useState(localStorageBooks);
     const [filteredFavouriteBooks, setFilteredFavouriteBooks] = useState([]);
     const [filteredSearchTerm, setFilteredSearchTerm] = useState("");
     const value = {
@@ -13,6 +17,11 @@ const FavouritesProvider = ({ children }) => {
         filteredSearchTerm,
         setFilteredSearchTerm,
     };
+
+    useEffect(() => {
+        localStorage.setItem("googleBooks", JSON.parse(favourites));
+    }, [favourites]);
+
     return (
         <FavouritesContext.Provider value={value}>
             {children}
