8de2e9934267f589227daf7227e65f9f34183ae9
xNapha <nathancai1997@gmail.com>
2023-05-08 17:57:07 +1000

    fixing 404 issue on github

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,11 @@ import App from "./App.jsx";
 import "./index.module.scss";
 import SearchQueryProvider from "./contexts/SearchQueryProvider.jsx";
 import BookProvider from "./contexts/BookProvider.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import FavouritesProvider from "./contexts/FavouritesProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <BrowserRouter basename="/google-books">
+    <HashRouter>
         <BookProvider>
             <SearchQueryProvider>
                 <FavouritesProvider>
@@ -16,5 +16,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
                 </FavouritesProvider>
             </SearchQueryProvider>
         </BookProvider>
-    </BrowserRouter>
+    </HashRouter>
 );
